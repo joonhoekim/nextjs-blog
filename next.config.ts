8709c01412f9361c5fbe5d644040bfda03743e00
26rote@gmail.com <26rote@gmail.com>
2024-11-26 16:19:01 +0000

    conf: google hosted image enabled

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // Enable static image imports
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
