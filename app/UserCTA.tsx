92a0ca69f31d90b2f14e8504c69da218422e8239
26rote@gmail.com <26rote@gmail.com>
2024-12-06 13:49:32 +0000

    main cta

---
+++ app/UserCTA.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from 'react';
+import Link from 'next/link';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import { Session } from 'next-auth';
+import { User } from '@prisma/client';
+
+interface UserCTAProps {
+    handle: string | null;
+}
+
+const UserCTA = ({ handle }: UserCTAProps) => {
+    if (!handle) {
+        return (
+            <h1>login first</h1>
+        )
+    }
+
+
+
+    return (
+        <Card className="m-4">
+            <div className="flex flex-col md:flex-row items-center justify-between gap-4 p-4">
+                <div>
+                    <h2 className="text-2xl font-bold mb-2">Welcome back!</h2>
+                    <p className="text-gray-600">YOUR HOME</p>
+                </div>
+
+                <Link href={`/${handle}`} >
+                    <Button
+                        label="USER_HOME"
+                        icon="pi pi-user"
+                        severity="info"
+                        raised
+                        className="p-button-lg"
+                    />
+                </Link>
+            </div>
+        </Card>
+    );
+};
+
+export default UserCTA;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/auth';
+import UserCTA from './UserCTA';
+import { prisma } from '@/lib/prisma';
+import type { User } from '@prisma/client';
 export default async function Home() {
+  const session = await getServerSession(authOptions);
+  const user = await prisma.user.findUnique({
+    where: { email: session?.user?.email! },
+  });
+  const handle = user?.handle
 
   return (
     <div>
       <h1>BLOG with nextjs</h1>
 
+
       <div>
         <p>여기에는 서비스 설명이 들어갈 예정임</p>
       </div>
+      <UserCTA handle={handle} />
     </div >
   );
 }
