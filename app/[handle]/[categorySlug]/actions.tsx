4ba8069913597a09508f623b15d203c6ff1f484d
26rote@gmail.com <26rote@gmail.com>
2024-12-13 23:53:55 +0900

    action: update post content

--- app/[handle]/[categorySlug]/actions.tsx
+++ app/[handle]/[categorySlug]/actions.tsx
@@ -6,23 +6,51 @@ import { slugify } from "@/lib/utils/slugify";
 import { revalidatePath } from "next/cache";
 
 export async function renamePost(postId: string, newTitle: string) {
-    const newSlug = slugify(newTitle);
+  const newSlug = slugify(newTitle);
 
-    try {
-        const post = await prisma.post.update({
-            where: { id: postId },
-            data: {
-                title: newTitle,
-                slug: newSlug,
-            },
-        });
+  try {
+    const post = await prisma.post.update({
+      where: { id: postId },
+      data: {
+        title: newTitle,
+        slug: newSlug,
+      },
+    });
 
-        // Revalidate the post list page
-        revalidatePath("/[handle]/[categorySlug]");
+    // Revalidate the post list page
+    revalidatePath("/[handle]/[categorySlug]");
 
-        return post;
-    } catch (error) {
-        console.error("Failed to rename post:", error);
-        throw new Error("Failed to rename post");
-    }
+    return post;
+  } catch (error) {
+    console.error("Failed to rename post:", error);
+    throw new Error("Failed to rename post");
+  }
+}
+
+export async function editPost(
+  categorySlug: string,
+  postSlug: string,
+  content: string,
+) {
+  // TODO: authentication required
+  const category = await prisma.category.findUnique({
+    where: { slug: categorySlug },
+    select: { id: true },
+  });
+
+  if (!category) {
+    // exception
+  }
+
+  const post = await prisma.post.update({
+    where: {
+      slug: postSlug,
+      categoryId: category.id,
+    },
+    data: {
+      content: content,
+    },
+  });
+
+  return post;
 }
